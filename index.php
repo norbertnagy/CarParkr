4c69e7d6662137b29ad9c26cdb5a6565c621309a
Norbert Nagy <roxanajula@Roxanas-MacBook-Pro.local>
2017-05-03 18:59:41 +0200

    Get JSON data + JSON decode

--- index.php
+++ index.php
@@ -7,6 +7,15 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+	<?php
+	include('class_request.php');
+	$request = new Request();
+
+	// Get JSON data from Open Data Aarhus and decode it
+	$data = json_decode(
+      $request->getFile("http://www.odaa.dk/api/action/datastore_search?resource_id=2a82a145-0195-4081-a13c-b0e587e9b89c")
+		);
+	?>
 <header>
 	<img src="logo.png">
 </header>
